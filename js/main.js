e45d1b732cc60bec8cfda9c625c6c90486b147a3
Nathan Howland <nathanhowland@KingWorm.local>
2026-05-29 15:39:09 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+if (toggle && navLinks) {
+    toggle.addEventListener('click', () => navLinks.classList.toggle('open'));
+    navLinks.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => navLinks.classList.remove('open'));
+    });
+}
